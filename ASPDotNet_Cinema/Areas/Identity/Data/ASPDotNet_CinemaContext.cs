82cbad8f1c4975fc3154217e98fb6ffc76a1b8a9
Wim Hambrouck <wim.hambrouck@ehb.be>
2022-06-13 12:14:02 +0200

    Identity

---
+++ ASPDotNet_Cinema/Areas/Identity/Data/ASPDotNet_CinemaContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPDotNet_Cinema.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPDotNet_Cinema.Data
+{
+    public class ASPDotNet_CinemaContext : IdentityDbContext<ASPDotNet_Gebruiker>
+    {
+        public ASPDotNet_CinemaContext(DbContextOptions<ASPDotNet_CinemaContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ASPDotNet_Cinema/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ASPDotNet_Cinema.Areas.Identity.Data;
+using ASPDotNet_Cinema.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASPDotNet_Cinema.Areas.Identity.IdentityHostingStartup))]
+namespace ASPDotNet_Cinema.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ASPDotNet_CinemaContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ASPDotNet_CinemaContextConnection")));
+
+                services.AddDefaultIdentity<ASPDotNet_Gebruiker>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ASPDotNet_CinemaContext>();
+            });
+        }
+    }
+}
