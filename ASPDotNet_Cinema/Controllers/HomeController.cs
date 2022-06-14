9379999b8ef79460e9b14f603e0c40276f343047
Wim Hambrouck <wim.hambrouck@ehb.be>
2022-06-14 18:57:29 +0200

    HomeController with redirect to MoviesController in case code refers to /Home

---
+++ ASPDotNet_Cinema/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNet_Cinema.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToActionPermanent(nameof(MoviesController.Index), nameof(MoviesController).Replace("Controller", ""));
+        }
+    }
+}
